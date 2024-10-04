a7cd5f756df5e5af1cc618da7dfdee1b621435bd
sheltonis <Sheltoncassell@gmail.com>
2024-10-04 00:56:35 -0500

    everything

--- app.py
+++ app.py
@@ -1,34 +1,5 @@
 from flask import Flask, request, Response
 import requests
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return '''
-    <h1>Surfyy</h1>
-    <form method="POST" action="/proxy">
-        <input type="text" name="url" placeholder="Enter URL to browse">
-        <input type="submit" value="Go">
-    </form>
-    '''
-
-@app.route('/proxy', methods=['POST'])
-def proxy():
-    url = request.form.get('url')
-    if not url.startswith('http://') and not url.startswith('https://'):
-        url = 'http://' + url
-
-    try:
-        response = requests.get(url)
-        return Response(response.content, content_type=response.headers['Content-Type'])
-    except requests.exceptions.RequestException as e:
-        return f"An error occurred: {e}"
-
-if __name__ == '__main__':
-    app.run(debug=True)
-from flask import Flask, request, Response
-import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
@@ -42,11 +13,11 @@ def home():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/static/styles.css">
-        <title>My Awesome Proxy</title>
+        <link rel="stylesheet" href="/static/styles.css">  <!-- Make sure this CSS file exists -->
+        <title>Surfyy</title>
     </head>
     <body>
-        <h1>My Awesome Proxy</h1>
+        <h1>Surfyy</h1>
         <form method="POST" action="/proxy">
             <input type="text" name="url" placeholder="Enter URL to browse" required>
             <input type="submit" value="Go">
@@ -68,11 +39,15 @@ def proxy():
         # Use BeautifulSoup to parse the HTML content
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Update all relative URLs in <a> and <img> tags
-        for tag in soup.find_all(['a', 'img']):
+        # Update all relative URLs in <a>, <img>, <link>, and <script> tags
+        for tag in soup.find_all(['a', 'img', 'link', 'script']):
             if tag.name == 'a':
                 tag['href'] = urljoin(url, tag.get('href'))
             elif tag.name == 'img':
+                tag['src'] = urljoin(url, tag.get('src'))
+            elif tag.name == 'link' and tag.get('href'):
+                tag['href'] = urljoin(url, tag.get('href'))
+            elif tag.name == 'script' and tag.get('src'):
                 tag['src'] = urljoin(url, tag.get('src'))
 
         return Response(str(soup), content_type=response.headers['Content-Type'])
